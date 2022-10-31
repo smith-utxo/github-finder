4f63170b60a3310d4a69e075b534049613a4e20b
Adam Smith <smith.adam937@gmail.com>
2022-10-31 14:51:20 -0500

    github api and user Results WIP

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <fragment>
+      <div className="h1 text-6xl mb-4">About this app</div>
+      <p>App to search github users</p>
+      <p>Version: 1.0.0</p>
+    </fragment>
+  )
+}
+
+export default About
---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserResults from '../Components/users/UserResults'
+
+function Home() {
+  return (
+    <>
+    {/*Search Component will go here */}
+      <UserResults />
+    </>
+  )
+}
+
+export default Home

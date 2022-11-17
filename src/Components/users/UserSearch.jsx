a0017c51cf5644d1c3eb73d403de31dc9285f0e8
Adam Smith <smith.adam937@gmail.com>
2022-11-16 19:21:35 -0600

    refactor with axios

--- src/Components/users/UserSearch.jsx
+++ src/Components/users/UserSearch.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import { useState, useContext } from 'react';
 import GithubContext from '../../context/github/GithubContext.jsx'
 import AlertContext from '../../context/alert/AlertContext.jsx'
-
+import {searchUsers} from '../../context/github/GithubActions'
 
 function UserSearch() {
   const [text, setText] = useState('')
 
-  const { users, searchUsers, clearUsers } = useContext(GithubContext)
+  const { users, dispatch, } = useContext(GithubContext)
   const {setAlert} = useContext(AlertContext)
 
-  const handleChange = (e) => {
-    setText(e.target.value)
-  }
-  const handleSubmit = (e) => {
+  const handleChange = (e) => setText(e.target.value)
+  
+  const handleSubmit = async (e) => {
     e.preventDefault()
     if (text === '') {
       setAlert('Please enter something!', 'error')
     } else {
+      dispatch({type: 'SET_LOADING'})
       //@TODO - SEARCH USERS 
-      searchUsers(text)
-
+      const users = await searchUsers(text)
+      dispatch({type: 'GET_USERS', payload: users})
       setText('')
     }
   }
@@ -41,7 +41,7 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-          <button onClick={clearUsers} className="btn- btn-ghost btn-lg rounded-md border-slate-200">Clear Search Results</button>
+          <button onClick={() => dispatch({type: 'CLEAR_USERS'})} className="btn- btn-ghost btn-lg rounded-md border-slate-200">Clear Search Results</button>
         </div>
       )}
     </div>
--- src/context/github/GithubReducer.jsx
+++ src/context/github/GithubReducer.jsx
@@ -1,32 +1,27 @@
 const githubReducer = (state, action) => {
   switch (action.type) {
-    case 'GET_USERS': 
-    return {
-      ...state, 
-      users: action.payload, 
-      loading: false,
-    }
-    case 'GET_USER': 
-    return {
-      ...state, 
-      user: action.payload, 
-      loading: false,
-    }
-    case 'SET_LOADING': 
-    return {
-      ...state, 
-      loading: true
-    }
-    case 'CLEAR_USERS': 
-    return {
-      ...state, 
-      users: [], 
-    }
-    case 'GET_REPOS':
+    case 'GET_USERS':
       return {
         ...state,
-        repos: action.payload, 
+        users: action.payload,
         loading: false,
+      }
+    case 'GET_USER_AND_REPOS':
+      return {
+        ...state,
+        user: action.payload.user,
+        repos: action.payload.repos,
+        loading: false,
+      }
+    case 'SET_LOADING':
+      return {
+        ...state,
+        loading: true
+      }
+    case 'CLEAR_USERS':
+      return {
+        ...state,
+        users: [],
       }
     default:
       return state
